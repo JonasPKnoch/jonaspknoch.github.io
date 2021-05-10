a666b3332b6e3211c492d6a2a44d4c333a3ebe48
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2021-05-10 13:20:20 -0600

    Updated stuff. Changed to .php

--- js/maze.js
+++ js/maze.js
@@ -2,30 +2,6 @@ cellSizePrefered = 6;
 
 setTimeout(init, 100);
 
-class Cell {
-	constructor(i, j) {
-		this.i = i;
-		this.j = j;
-		this.up = null;
-		this.down = null;
-		this.left = null;
-		this.right = null;
-	}
-
-	draw() {
-		ctx.rect(this.i*cellSize + cellSize*0.5, this.j*cellSize + cellSize*0.5, cellSize, cellSize);
-
-		if(this.left != null)
-			ctx.rect(this.i*cellSize + cellSize*0.5, this.j*cellSize + cellSize*0.5, cellSize, cellSize);
-		if(this.right != null)
-			ctx.rect(this.i*cellSize + cellSize*0.5, this.j*cellSize + cellSize*0.5, cellSize, cellSize);
-		if(this.up != null)		
-			ctx.rect(this.i*cellSize + cellSize*0.5, this.j*cellSize + cellSize*0.5, cellSize, cellSize);
-		if(this.down != null)
-			ctx.rect(this.i*cellSize + cellSize*0.5, this.j*cellSize + cellSize*0.5, cellSize, cellSize);
-	}
-}
-
 function init() {
 	c = document.getElementById("canvas");
 	ctx = c.getContext("2d");
@@ -48,19 +24,20 @@ function init() {
 	
 	stampObject(document.getElementById("content"));
 
-	openCells = [[[Math.round(width/2), 10],[Math.round(width/2), 10]]];
+	openCells = [[[Math.round(width/2), 9],[Math.round(width/2), 9]]];
 	finish = false;
 	ctx.fillStyle = "#c0c0c0";
+	ctx.strokeStyle = "#c0c0c0";
 	loop();
 }
 
 function stampObject(object) {
 	let rect = object.getBoundingClientRect();
-	let gridILow = Math.round(rect.left/cellSize) - 1;
-	let gridJLow = Math.round(rect.top/cellSize) - 1;
-	let gridIHigh = Math.round(rect.right/cellSize) + 1;
-	let gridJHigh = Math.round(rect.bottom/cellSize) + 1;
-	console.log(rect.height);
+	let gridILow = Math.round(object.offsetLeft/cellSize);
+	let gridJLow = Math.round(object.offsetTop/cellSize);
+	let gridIHigh = Math.round((object.offsetLeft + object.offsetWidth)/cellSize);
+	let gridJHigh = Math.round((object.offsetTop + object.offsetHeight)/cellSize);
+	console.log(object.offsetTop);
 
 	for(let i = 0; i < gridIHigh - gridILow; i++) {
 		for(let j = 0; j < gridJHigh - gridJLow; j++) {
@@ -70,8 +47,6 @@ function stampObject(object) {
 }
 
 function loop() {
-	step();
-	step();
 	step();
 	if(!finish)
 		setTimeout(loop, 1);
@@ -84,37 +59,80 @@ function step() {
 			return;
 		}
 		
-		nextIndex = openCells.length - Math.floor(Math.random() * Math.min(openCells.length, 32)) - 1;
+		nextIndex = openCells.length - Math.floor(Math.random() * Math.min(openCells.length, 16)) - 1;
 		next = openCells[nextIndex];
-		nextI = next[1][0];
-		nextJ = next[1][1];
-		pathI = next[0][0];
-		pathJ = next[0][1];
-		if(!openAt(nextI, nextJ)) {
+		if(!openAt(next[1])) {
 			openCells.splice(nextIndex, 1);
 		}
-	} while(!openAt(nextI, nextJ));
+	} while(!openAt(next[1]));
 	
-	grid[nextI][nextJ] = 1;
-	grid[pathI][pathJ] = 1;
-	drawAt(nextI, nextJ);
-	drawAt(pathI, pathJ);
+	openPath(next);
 	
-	if(openAt(nextI + 2, nextJ)) {
-		openCells.push([[nextI + 1, nextJ],[nextI + 2, nextJ]]);
+	if(openAt(leftOf(next[1]))) {
+		openCells.push(getPath(leftPathOf(next[1]), leftOf(next[1])));
 	}
-	if(openAt(nextI - 2, nextJ)) {
-		openCells.push([[nextI - 1, nextJ],[nextI - 2, nextJ]]);
+	if(openAt(rightOf(next[1]))) {
+		openCells.push(getPath(rightPathOf(next[1]), rightOf(next[1])));
 	}
-	if(openAt(nextI, nextJ + 2)) {
-		openCells.push([[nextI, nextJ + 1],[nextI, nextJ + 2]]);
+	if(openAt(upOf(next[1]))) {
+		openCells.push(getPath(upPathOf(next[1]), upOf(next[1])));
 	}
-	if(openAt(nextI, nextJ - 2)) {
-		openCells.push([[nextI, nextJ - 1],[nextI, nextJ - 2]]);
+	if(openAt(downOf(next[1]))) {
+		openCells.push(getPath(downPathOf(next[1]), downOf(next[1])));
 	}
 }
 
-function openAt(i, j) {
+function openPath(path) {
+	grid[path[0][0]][path[0][1]] = 1;
+	grid[path[1][0]][path[1][1]] = 1;
+	drawAt(path[0]);
+	drawAt(path[1]);
+}
+
+function getPath(from, to) {
+	return [from, to];
+}
+
+function leftOf(point) {
+	return adj(point, 2, 0);
+}
+
+function rightOf(point) {
+	return adj(point, -2, 0);
+}
+
+function upOf(point) {
+	return adj(point, 0, 2);
+}
+
+function downOf(point) {
+	return adj(point, 0, -2);
+}
+
+function leftPathOf(point) {
+	return adj(point, 1, 0);
+}
+
+function rightPathOf(point) {
+	return adj(point, -1, 0);
+}
+
+function upPathOf(point) {
+	return adj(point, 0, 1);
+}
+
+function downPathOf(point) {
+	return adj(point, 0, -1);
+}
+
+
+function adj(point, i, j) {
+	return [point[0] + i, point[1] + j]
+}
+
+function openAt(point) {
+	let i = point[0];
+	let j = point[1];
 	if(i < 0 || width <= i || j < 0 || height <= j)
 		return false;
 	
@@ -133,10 +151,13 @@ function drawAll() {
 	ctx.fill();
 }
 
-function drawAt(i, j) {
+function drawAt(point) {
+	let i = point[0];
+	let j = point[1];
 	if(grid[i][j] == 1) {
 		ctx.beginPath();
 		ctx.rect(i*cellSize, j*cellSize, cellSize, cellSize);
 		ctx.fill();
+		ctx.stroke();
 	}
 }
